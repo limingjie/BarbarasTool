613dcd9837991332d5890b5dc034cc113cd8eead
Mingjie Li <mingjie.li@everbridge.com>
2019-12-26 16:51:44 +0800

    Reword and only update changed values

--- main.go
+++ main.go
@@ -14,42 +14,46 @@ import (
 var mainwin *ui.Window
 
 func setupUI() {
-	mainwin = ui.NewWindow("Barbara's Tool", 640, 100, true)
+	mainwin = ui.NewWindow("Barbara's Tool", 700, 100, true)
+
 	mainwin.OnClosing(func(*ui.Window) bool {
 		ui.Quit()
 		return true
 	})
+
 	ui.OnShouldQuit(func() bool {
 		mainwin.Destroy()
 		return true
 	})
 
-	// Main layout
-	box := ui.NewVerticalBox()
-	box.SetPadded(true)
+	// Controls
+	vendorEntry := ui.NewEntry()
+	vendorEntry.SetReadOnly(true)
+	bidEntry := ui.NewEntry()
+	bidEntry.SetReadOnly(true)
+	buttonOpenFile := ui.NewButton("Open Files")
+	buttonProcess := ui.NewButton("Process")
 
 	// Filename form
 	form := ui.NewForm()
 	form.SetPadded(true)
-	input := ui.NewEntry()
-	input.SetReadOnly(true)
-	output := ui.NewEntry()
-	output.SetReadOnly(true)
 	form.Append("", ui.NewLabel("Search Price (H) & Lead time (L) base on P/N (C)"), false)
-	form.Append("Vendor", input, false)
+	form.Append("Vendor", vendorEntry, false)
 	form.Append("", ui.NewLabel("Fill in Price (K) & Lead time (P) base on P/N (F)"), false)
-	form.Append("Bid", output, false)
-	box.Append(form, false)
+	form.Append("Bid", bidEntry, false)
 
 	// Buttons
 	buttonBox := ui.NewHorizontalBox()
 	buttonBox.SetPadded(true)
-	buttonOpenFile := ui.NewButton("Open Files")
-	buttonProcess := ui.NewButton("Process")
 	buttonBox.Append(ui.NewLabel(""), true)
-	buttonBox.Append(ui.NewLabel("Hey Barbara! Calm down..."), false)
+	buttonBox.Append(ui.NewLabel("Cheers Barbara!"), false)
 	buttonBox.Append(buttonOpenFile, false)
 	buttonBox.Append(buttonProcess, false)
+
+	// Main layout
+	box := ui.NewVerticalBox()
+	box.SetPadded(true)
+	box.Append(form, false)
 	box.Append(buttonBox, false)
 
 	// Main window
@@ -59,70 +63,70 @@ func setupUI() {
 
 	// Open file button click event
 	buttonOpenFile.OnClicked(func(*ui.Button) {
-		input.SetText(ui.OpenFile(mainwin))
-		output.SetText(ui.OpenFile(mainwin))
+		vendorEntry.SetText(ui.OpenFile(mainwin))
+		bidEntry.SetText(ui.OpenFile(mainwin))
 	})
 
 	// Process button click event
 	buttonProcess.OnClicked(func(*ui.Button) {
-		// Check input filename
-		inputFilename := strings.TrimSpace(input.Text())
-		if len(inputFilename) == 0 {
-			ui.MsgBox(mainwin, "Error", "Input filename cannot be empty.")
+		// Check vendor filename
+		vendorFilename := strings.TrimSpace(vendorEntry.Text())
+		if len(vendorFilename) == 0 {
+			ui.MsgBox(mainwin, "Error", "Vendor filename cannot be empty.")
 			return
 		}
-		if !strings.HasSuffix(inputFilename, ".xlsx") {
+		if !strings.HasSuffix(vendorFilename, ".xlsx") {
 			ui.MsgBox(mainwin, "Error", "Only support xlsx format :)")
 			return
 		}
 
-		// Check output filename
-		outputFilename := strings.TrimSpace(output.Text())
-		if len(outputFilename) == 0 {
-			ui.MsgBox(mainwin, "Error", "Output filename cannot be empty.")
+		// Check bid filename
+		bidFilename := strings.TrimSpace(bidEntry.Text())
+		if len(bidFilename) == 0 {
+			ui.MsgBox(mainwin, "Error", "Bid filename cannot be empty.")
 			return
 		}
-		if !strings.HasSuffix(outputFilename, ".xlsx") {
+		if !strings.HasSuffix(bidFilename, ".xlsx") {
 			ui.MsgBox(mainwin, "Error", "Only support xlsx format :)")
 			return
 		}
 
-		// Open input file
-		inputFile, err := excelize.OpenFile(inputFilename)
+		// Open vendor file
+		vendorFile, err := excelize.OpenFile(vendorFilename)
 		if err != nil {
 			ui.MsgBox(mainwin, "Error", err.Error())
 			return
 		}
 
-		// Open output file
-		outputFile, err := excelize.OpenFile(outputFilename)
+		// Open bid file
+		bidFile, err := excelize.OpenFile(bidFilename)
 		if err != nil {
 			ui.MsgBox(mainwin, "Error", err.Error())
 			return
 		}
 
-		// Make sure input file has at least 1 data sheet
-		sheets := inputFile.GetSheetMap()
+		// Make sure vendor file has at least 1 data sheet
+		sheets := vendorFile.GetSheetMap()
 		if len(sheets) == 0 {
-			ui.MsgBox(mainwin, "Error", "The vendor excel has no sheet!")
+			ui.MsgBox(mainwin, "Error", "The vendor file is empty!")
 			return
 		}
 
-		// Create data maps by reading all input file sheet(s)
+		// Create data maps by reading all vendor file sheet(s)
 		// - PN        - Col C - "型号（P/N)"
 		// - Price     - Col H - "合计（不含税）"
 		// - Lead time - Col L - "货期"
 		priceMap := make(map[string]string)
 		leadTimeMap := make(map[string]string)
 		for _, sheetName := range sheets {
-			rows, err := inputFile.GetRows(sheetName)
+			rows, err := vendorFile.GetRows(sheetName)
 			if err != nil {
 				ui.MsgBox(mainwin, "Error", err.Error())
 				continue
 			}
 
 			for i := 0; i < len(rows); i++ {
-				// Make sure pn & price exists
+				// Make sure PN & price exists
 				if len(rows[i]) < 8 {
 					continue
 				}
@@ -144,7 +148,7 @@ func setupUI() {
 					continue
 				}
 
-				// Check and set leadtime
+				// Check and set lead time
 				leadTime := strings.TrimSpace(rows[i][11])
 				if len(leadTime) != 0 {
 					leadTime = strings.ReplaceAll(leadTime, "周", " wks")
@@ -160,32 +164,35 @@ func setupUI() {
 			return
 		}
 
-		// Make sure output file has at least 1 data sheet
-		sheets = outputFile.GetSheetMap()
+		// Make sure bid file has at least 1 data sheet
+		sheets = bidFile.GetSheetMap()
 		if len(sheets) == 0 {
-			ui.MsgBox(mainwin, "Error", "The bid excel has no sheet!")
+			ui.MsgBox(mainwin, "Error", "The bid excel is empty!")
 			return
 		}
 
-		// Backup output file
-		outputFile.SaveAs("./backup." + time.Now().Format("20060102-150405-0700") + ".xlsx")
+		// Backup bid file
+		bidFile.SaveAs("./backup." + time.Now().Format("20060102.150405.999999") + ".xlsx")
 
-		// Update output file
-		updateCounter := 0
+		// Update bid file
+		pnFoundMap := make(map[string]bool)
+		priceUpdateCounter := 0
+		leadTimeUpdatecounter := 0
 		for _, sheetName := range sheets {
-			rows, err := outputFile.GetRows(sheetName)
+			rows, err := bidFile.GetRows(sheetName)
 			if err != nil {
 				ui.MsgBox(mainwin, "Error", err.Error())
 				continue
 			}
 
-			// Write output file
+			// Update bid file
 			// - PN        - Col F - International Part No.
 			// - Price     - Col K - Unit Price CNY
 			// - Lead time - Col P - leadtime
 			for i := 0; i < len(rows); i++ {
 				// Make sure F exists
-				if len(rows[i]) < 6 {
+				length := len(rows[i])
+				if length < 6 {
 					continue
 				}
 
@@ -197,37 +204,42 @@ func setupUI() {
 
 				// Update price
 				if price, ok := priceMap[pn]; ok {
-					if cellName, err := excelize.CoordinatesToCellName(11, i+1); err == nil {
-						outputFile.SetCellDefault(sheetName, cellName, price)
-						updateCounter++
+					pnFoundMap[pn] = true
+					if length < 11 || price != rows[i][10] {
+						if cellName, err := excelize.CoordinatesToCellName(11, i+1); err == nil {
+							bidFile.SetCellDefault(sheetName, cellName, price)
+							priceUpdateCounter++
+						}
 					}
 				}
 
 				// Update lead time
-				if leadtime, ok := leadTimeMap[pn]; ok {
-					if cellName, err := excelize.CoordinatesToCellName(16, i+1); err == nil {
-						outputFile.SetCellStr(sheetName, cellName, leadtime)
-						// outputFile.SetCellDefault(sheetName, cellName, leadtime)
-						updateCounter++
+				if leadTime, ok := leadTimeMap[pn]; ok {
+					pnFoundMap[pn] = true
+					if length < 16 || leadTime != rows[i][15] {
+						if cellName, err := excelize.CoordinatesToCellName(16, i+1); err == nil {
+							bidFile.SetCellStr(sheetName, cellName, leadTime)
+							leadTimeUpdatecounter++
+						}
 					}
 				}
 			}
 		}
 
-		// Nothing updated
-		if updateCounter == 0 {
-			ui.MsgBox(mainwin, "Message", "Nothing updated.")
-			return
-		}
-
-		// Save file
-		if err := outputFile.Save(); err != nil {
-			ui.MsgBox(mainwin, "Error", "Save output excel.\n"+err.Error())
-			return
+		// Save bid file if updated
+		if priceUpdateCounter != 0 || leadTimeUpdatecounter != 0 {
+			if err := bidFile.Save(); err != nil {
+				ui.MsgBox(mainwin, "Error", "Error save bid file.\n"+err.Error())
+				return
+			}
 		}
 
 		// Done
-		ui.MsgBox(mainwin, "Complete", fmt.Sprintf("Done! %d cells updated!", updateCounter))
+		ui.MsgBox(mainwin, "Done!",
+			fmt.Sprintf("%d matching PN(s) found from vendor file.\n"+
+				"%d price cell(s) updated.\n"+
+				"%d lead time cell(s) updated.",
+				len(pnFoundMap), priceUpdateCounter, leadTimeUpdatecounter))
 	})
 }
 
